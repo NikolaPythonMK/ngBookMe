457eb118d494ee17823f097b7c76db88d0814bbf
Jovan Stojcev <stojcevjovan@hotmail.com>
2023-09-14 18:34:27 +0200

    Added calednar functionalitY

--- src/app/components/property-details/property-details.component.ts
+++ src/app/components/property-details/property-details.component.ts
@@ -1,109 +1,79 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit, ViewEncapsulation} from "@angular/core";
 import {Property} from "../../models/Property";
 import {PropertyService} from "../../services/PropertyService";
 import {ActivatedRoute} from "@angular/router";
 import { Location } from '@angular/common';
-import { NgbDate, NgbCalendar, NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
-import { JsonPipe } from '@angular/common';
-
+import { NgbDate, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
+import { DateRange, MAT_RANGE_DATE_SELECTION_MODEL_PROVIDER } from '@angular/material/datepicker';
+import {MatCalendarCellClassFunction, MatDatepickerModule} from '@angular/material/datepicker';
+import { from } from "rxjs";
 
 @Component({
   selector: 'component-details',
   templateUrl: './property-details.component.html',
-  styleUrls: ['./property-details.component.css']
+  styleUrls: ['./property-details.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class PropertyDetailsComponent implements OnInit{
 
-  hoveredDate: NgbDate | null = null;
-
-  fromDate: NgbDate;
-  toDate: NgbDate | null = null;
-
-  readonly date1 = {
-    year: 2023,
-    month: 10,
-    day: 11
-  } as NgbDate;
-
-  readonly date2 = {
-    year: 2023,
-    month: 10,
-    day: 15
-  } as NgbDate;
-
-  // constructor(calendar: NgbCalendar) {
-  //   this.fromDate = calendar.getToday();
-  //   this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
+  // onDateSelection(date: NgbDate) {
+  //   if (!this.fromDate && !this.toDate) {
+  //     this.fromDate = date;
+  //   } else if (this.fromDate && !this.toDate && date.after(this.fromDate)) {
+  //     this.toDate = date;
+  //   } else {
+  //     this.toDate = null;
+  //     this.fromDate = date;
+  //   }
   // }
 
-  onDateSelection(date: NgbDate) {
-    if (!this.fromDate && !this.toDate) {
-      this.fromDate = date;
-    } else if (this.fromDate && !this.toDate && date.after(this.fromDate)) {
-      this.toDate = date;
-    } else {
-      this.toDate = null;
-      this.fromDate = date;
+  // isHovered(date: NgbDate) {
+  //   return (
+  //     this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate)
+  //   );
+  // }
+
+  // isInside(date: NgbDate) {
+  //   return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
+  // }
+
+  // isRange(date: NgbDate) {
+  //   return (
+  //     date.equals(this.fromDate) ||
+  //     (this.toDate && date.equals(this.toDate)) ||
+  //     this.isInside(date) ||
+  //     this.isHovered(date)
+  //   );
+  // }
+
+  // isUsedRange(date: NgbDate) {
+  //   return(
+  //     (date.after(this.date1) || date.equals(this.date1)) && (date.before(this.date2) || date.equals(this.date2))
+  //   )
+  // }
+  dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
+    if (view === 'month') {
+      const date = cellDate.getDate();
+      return date >= 14 && date <= 20 ? 'example-custom-date-class' : '';
     }
-  }
-
-  isHovered(date: NgbDate) {
-    return (
-      this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate)
-    );
-  }
-
-  isInside(date: NgbDate) {
-    return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
-  }
-
-  isRange(date: NgbDate) {
-    return (
-      date.equals(this.fromDate) ||
-      (this.toDate && date.equals(this.toDate)) ||
-      this.isInside(date) ||
-      this.isHovered(date)
-    );
-  }
-
-  isUsedRange(date: NgbDate) {
-    return(
-      (date.after(this.date1) || date.equals(this.date1)) && (date.before(this.date2) || date.equals(this.date2))
-    )
-  }
-
-  test(date: NgbDate): void{
-
-  }
+    return '';
+  };
 
   property!: Property;
   images: string[] = [];
   first: boolean = true;
   activeSlideIndex: number = 0;
+  selectedRange: DateRange<Date> | any = null;
+  fromDate:  Date | any = null;
+  toDate: Date | any = null;
 
   constructor(private propertyService: PropertyService,
               private route: ActivatedRoute,
-              private location: Location,
-              private calendar: NgbCalendar) {
-    this.fromDate = calendar.getToday();
-    this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
-
+              private location: Location) {
+    this.refreshDR();
   }
 
   ngOnInit(): void {
-
-    const date1 = {
-      year: 2023,
-      month: 10,
-      day: 11
-    } as NgbDate;
-
-    const date2 = {
-      year: 2023,
-      month: 10,
-      day: 20
-    } as NgbDate;
-
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.propertyService.getById(id).subscribe({
       next: (property) => {
@@ -116,6 +86,25 @@ export class PropertyDetailsComponent implements OnInit{
         console.log(err);
       }
     })
+  }
+
+  refreshDR() {
+    this.selectedRange = new DateRange(this.fromDate, this.toDate);
+  }
+
+  onChange(date: any) {
+    if (!this.fromDate && !this.toDate) {
+      this.fromDate = date;
+    } else if (this.fromDate && !this.toDate) {
+      this.toDate = date;
+    } else {
+      this.toDate = null;
+      this.fromDate = date;
+    }
+
+    if(this.toDate !== null && this.fromDate !==null){
+      this.refreshDR();
+    }
   }
 
   goBack(): void{
