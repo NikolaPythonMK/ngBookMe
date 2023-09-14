c4312eb68150e51c7aa11ecab3f41439bee52c51
NikolaPythonMk <nikola.python159@gmail.com>
2023-09-14 19:59:06 +0200

    added tabs

--- src/app/components/display-history/display-history.component.ts
+++ src/app/components/display-history/display-history.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from "@angular/core";
 import {RecentlyViewedPage} from "../../models/RecentlyViewedPage";
 import {RecentlyViewedService} from "../../services/RecentlyViewedService";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'display-history-app',
@@ -10,7 +11,8 @@ import {RecentlyViewedService} from "../../services/RecentlyViewedService";
 export class DisplayHistoryComponent implements OnInit{
   page!: RecentlyViewedPage;
 
-  constructor(private recentlyViewedService: RecentlyViewedService,) {}
+  constructor(private recentlyViewedService: RecentlyViewedService,
+              private _snackBar: MatSnackBar,) {}
 
   ngOnInit(): void {
     this.recentlyViewedService.getAll().subscribe({
@@ -22,5 +24,17 @@ export class DisplayHistoryComponent implements OnInit{
         console.log(err)
       }
     })
+  }
+
+  bookmarkSnackbar(text: string): void{
+    this._snackBar.open(text, 'Close', {
+      horizontalPosition: 'start',
+      verticalPosition: 'bottom',
+      duration: 3000
+    })
+  }
+
+  deleteAll(): void{
+
   }
 }
